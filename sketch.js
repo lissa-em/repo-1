2a9c2587394a68000c5ced9c111c6537af83d957
lissa-em <aliciahackett02@gmail.com>
2024-12-10 15:22:55 +0000

    Face with textbox
    avatar 1 : happy, filler text

--- sketch.js
+++ sketch.js
@@ -3,23 +3,34 @@ function setup() {
 }
 
 function draw() {
+
+  //Smile
   background(200,255,200);
   //Border
   stroke('green');
   strokeWeight(3);
   noFill();
   square(30,30,340);
-  // eyes
+  //Eyes
   stroke('green');
   fill('green');
   square(90,90,40);
   square(270,90,40);
-  //mouth
+  //Mouth
   stroke('green');
   fill('green');
   square(90,200,40);
   rect(130,240,140,40);
   square(270,200,40);
+  //Text 
+  stroke(230,255,232);
+  fill(230,255,232);
+  rect(50,320,300,30);
+  fill('green');
+  textSize(20);
+  textFont('Courier New');
+  noStroke();
+  text(">>text here", 60,340);
 
 
 }
